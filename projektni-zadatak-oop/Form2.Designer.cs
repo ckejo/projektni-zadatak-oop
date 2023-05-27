91ae58a37c7b1e96534bc53a79278defd9a5076e
ckejo <jocic.andrija360@gmail.com>
2023-05-27 15:18:53 +0200

    working projectiles

--- projektni-zadatak-oop/Form2.Designer.cs
+++ projektni-zadatak-oop/Form2.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.components = new System.ComponentModel.Container();
+            this.tProjectileMover = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // tProjectileMover
+            // 
+            this.tProjectileMover.Interval = 50;
+            this.tProjectileMover.Tick += new System.EventHandler(this.tProjectileMover_Tick);
             // 
             // Form2
             // 
@@ -37,16 +43,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(815, 803);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
             this.Text = "Galaxigan";
             this.Load += new System.EventHandler(this.Form2_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form2_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form2_KeyPress);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer tProjectileMover;
     }
 }
--- projektni-zadatak-oop/Form2.cs
+++ projektni-zadatak-oop/Form2.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,29 +19,93 @@ namespace projektni_zadatak_oop
             InitializeComponent();
         }
 
+        Character player;
+        Projectile playerProjectile;
+        bool projectileExists = false;
+
         private void Form2_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Space)
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
             {
-                Projectile playerProjecile = new Projectile(1);
+                if (player.x - 2 < 0) { }
+                else
+                {
+                    Refresh();
+                    player.Move(-5, 0);
+                }
+            }
+            
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+            {
+                if (player.x + 2 > ClientRectangle.Width) { }
+                else
+                {
+                    Refresh();
+                    player.Move(5, 0);
+                }
+            }
+            
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                if (player.y + 2 > ClientRectangle.Height) { }
+                else
+                {
+                    Refresh();
+                    player.Move(0, -5);
+                }
+            }
+            
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+            {
+                if (player.y - 2 < 0) { }
+                else
+                {
+                    Refresh();
+                    player.Move(0, 5);
+                }
+            }
+
+            if (e.KeyCode == Keys.Space || e.KeyCode == Keys.Z)
+            {
+                playerProjectile.Set(player.x + 5, player.y, playerProjectile.strength);
+                Graphics g = CreateGraphics();
+                projectileExists = true;
+                tProjectileMover.Enabled = true;
+                playerProjectile.Draw(g);
             }
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            Character player = new Character(ClientRectangle.Width / 2, ClientRectangle.Height - 50);
-            player.Draw();
+            player = new Character(ClientRectangle.Width / 2 - 5, ClientRectangle.Height - 50);
+            playerProjectile = new Projectile(player.x + 5, player.y, 2);
         }
 
-        private void Form2_KeyPress(object sender, KeyPressEventArgs e)
+        private void Form2_Paint(object sender, PaintEventArgs e)
         {
-            
+            if (projectileExists) playerProjectile.Draw(e.Graphics);
+            player.Draw(e.Graphics);         
+        }
+
+        private void tProjectileMover_Tick(object sender, EventArgs e)
+        {
+            if (playerProjectile.y < 0)
+            {
+                projectileExists = false;
+                tProjectileMover.Enabled = false;
+            }
+
+            Refresh();
+            playerProjectile.Move(0, 20);
         }
     }
 
     public class Character
     {
-        int x, y;
+        public int x { get; set; }
+        public int y { get; set; }
+       
+
         public Character(int x, int y) 
         {
             this.x = x;
@@ -49,30 +114,70 @@ namespace projektni_zadatak_oop
     
         public void Move(int dx, int dy)
         {
-
+            this.x += dx;
+            this.y += dy;
         }
 
-        public void Draw()
+        public void Draw(Graphics g)
         {
-            PictureBox pbPlayer = new PictureBox();
-            pbPlayer.Location = new Point(x, y);
-            pbPlayer.Image = Image.FromFile("C:\\Users\\ck3jo\\Documents\\GitHub\\projektni-zadatak-oop\\projektni-zadatak-oop\\player.jpg");
-            pbPlayer.Show();
+            SolidBrush rBrush = new SolidBrush(Color.Red);
+            SolidBrush bBrush = new SolidBrush(Color.Blue);
+
+            g.FillRectangle(rBrush, x, y, 10, 10);
         }
     }
 
     public class Projectile
     {
-        int strength;
+        public int x { get; set; }
+        public int y { get; set; }
+        public int strength { get; set; }
 
-        public Projectile(int strength)
+        public Projectile(int x, int y, int strength)
         {
+            this.x = x;
+            this.y = y;
+            this.strength = strength;
+        }
+
+        public void Set(int x, int y, int strength)
+        {
+            this.x = x;
+            this.y = y;
             this.strength = strength;
         }
 
         public void IncreaseStrength(int d)
         {
             strength += d;
+        }
+
+        public void Move(int dx, int dy)
+        {
+            this.x -= dx;
+            this.y -= dy;
+        }
+
+        public void Draw(Graphics g)
+        {
+            Pen yPen = new Pen(new SolidBrush(Color.Yellow), 2);
+            Pen gPen = new Pen(new SolidBrush(Color.Green), 2);
+            Pen pPen = new Pen(new SolidBrush(Color.Purple), 2);
+
+            switch (strength)
+            {
+                case 1:
+                    g.DrawLine(yPen, new Point(x, y - 2), new Point(x, y - 30));
+                    break;
+                case 2:
+                    g.DrawLine(gPen, new Point(x, y - 2), new Point(x, y - 30));
+                    break;
+                case 3:
+                    g.DrawLine(pPen, new Point(x, y - 2), new Point(x, y - 30));
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
